482b67d3084ee6de52fd84afd908c796846ff6e9
Pedro Paulo Moraes <pedropaulo.moraes@technipfmc.com>
2023-01-16 11:51:09 -0300

    Algumas correções e melhorias

--- scripts/game.js
+++ scripts/game.js
@@ -1,74 +1,74 @@
 let perguntas_Nvl1 = [
     {
         pergunta: "Qual é o coletivo de cães?",
-        escolha1: "1- Matilha",
-        escolha2: "2- Rebanho",
-        escolha3: "3- Cardume",
-        escolha4: "4- Manada",
+        escolha1: "Matilha",
+        escolha2: "Rebanho",
+        escolha3: "Cardume",
+        escolha4: " Manada",
         resposta: 1
     },
     {
         pergunta: "Qual casal foi expluso do Paraíso?",
-        escolha1: "1- Sansão e Dalila",
-        escolha2: "2- José e Maria",
-        escolha3: "3- Sara e Abraão",
-        escolha4: "4- Adão e Eva",
+        escolha1: "Sansão e Dalila",
+        escolha2: "José e Maria",
+        escolha3: "Sara e Abraão",
+        escolha4: " Adão e Eva",
         resposta: 4
     },
     {
         pergunta: "Segundo os contos, qual animal ao ser beijado se transforma em príncipe?",
-        escolha1: "1- Cavalo",
-        escolha2: "2- Cão",
-        escolha3: "3- Gato",
-        escolha4: "4- Sapo",
+        escolha1: "Cavalo",
+        escolha2: "Cão",
+        escolha3: "Gato",
+        escolha4: " Sapo",
         resposta: 4
     },
     {
         pergunta: "Quem é o primeiro substituto do presidente?",
-        escolha1: "1- Vereador",
-        escolha2: "2- Vice-presidente",
-        escolha3: "3- Senador",
-        escolha4: "4- Deputado",
+        escolha1: "Vereador",
+        escolha2: "Vice-presidente",
+        escolha3: "Senador",
+        escolha4: " Deputado",
         resposta: 2
     },
     {
         pergunta: "Que animal é o Pateta?",
-        escolha1: "1- Cavalo",
-        escolha2: "2- Burro",
-        escolha3: "3- Cachorro",
-        escolha4: "4- Raposa",
+        escolha1: "Cavalo",
+        escolha2: "Burro",
+        escolha3: "Cachorro",
+        escolha4: " Raposa",
         resposta: 3
     },
     {
         pergunta: "Qual o número mínimo que um mês pode ter?",
-        escolha1: "1- 28",
-        escolha2: "2- 29",
-        escolha3: "3- 30 ",
-        escolha4: "4- 31",
+        escolha1: "28",
+        escolha2: "29",
+        escolha3: "30 ",
+        escolha4: " 31",
         resposta: 1
     },
     {
         pergunta: "Que nome se da à mistura de água com sal?",
-        escolha1: "1- Salmora",
-        escolha2: "2- Gangorra",
-        escolha3: "3- Melado",
-        escolha4: "4- Bolacha",
+        escolha1: "Salmora",
+        escolha2: "Gangorra",
+        escolha3: "Melado",
+        escolha4: " Bolacha",
         resposta: 1
     },
     {
         pergunta: "De onde as abelhas extraem mel?",
-        escolha1: "1- Das rochas",
-        escolha2: "2- Das flores",
-        escolha3: "3- Da água",
-        escolha4: "4- Da terra",
+        escolha1: "Das rochas",
+        escolha2: "Das flores",
+        escolha3: "Da água",
+        escolha4: " Da terra",
         resposta: 2
     },
     {
         pergunta: "Como se chama o ratinho criado por Walt Disney?",
-        escolha1: "1- Jerry",
-        escolha2: "2- Pernalonga",
-        escolha3: "3- Frajola",
-        escolha4: "4- Mickey Mouse",
+        escolha1: "Jerry",
+        escolha2: "Pernalonga",
+        escolha3: "Frajola",
+        escolha4: " Mickey Mouse",
         resposta: 4
     }
 ];
@@ -76,66 +76,66 @@ let perguntas_Nvl1 = [
 let perguntas_Nvl2 = [
     {
         pergunta: "Qual foi o último estado criado no Brasil?",
-        escolha1: "1- Tocantins",
-        escolha2: "2- Acre",
-        escolha3: "3- Rondônia",
-        escolha4: "4- Mato Grosso do Sul",
+        escolha1: "Tocantins",
+        escolha2: "Acre",
+        escolha3: "Rondônia",
+        escolha4: " Mato Grosso do Sul",
         resposta: 1
     },
     {
         pergunta: "Em que país nasceu Carmem Miranda?",
-        escolha1: "1- Brasil",
-        escolha2: "2- Espanha",
-        escolha3: "3- Portugal",
-        escolha4: "4- Argentina",
+        escolha1: "Brasil",
+        escolha2: "Espanha",
+        escolha3: "Portugal",
+        escolha4: " Argentina",
         resposta: 3
     },
     {
         pergunta: "Quem escreveu o livro 'O Guarani'?",
-        escolha1: "1- Carlos Gomes",
-        escolha2: "2- Olavo Billac",
-        escolha3: "3- José de Alencar",
-        escolha4: "4- Machado de Assis",
+        escolha1: "Carlos Gomes",
+        escolha2: "Olavo Billac",
+        escolha3: "José de Alencar",
+        escolha4: " Machado de Assis",
         resposta: 3
     },
     {
         pergunta: "Quem foi o amor de Isolda na Obra Medieval?",
-        escolha1: "1- Romeu",
-        escolha2: "2- Tristão",
-        escolha3: "3- Rigoleto",
-        escolha4: "4- Hamlet",
+        escolha1: "Romeu",
+        escolha2: "Tristão",
+        escolha3: "Rigoleto",
+        escolha4: " Hamlet",
         resposta: 2
     },
     {
         pergunta: "Como eram chamados os reis do antigo Egito?",
-        escolha1: "1- Generais",
-        escolha2: "2- Faraós",
-        escolha3: "3- Sheiks",
-        escolha4: "4- Presidentes",
+        escolha1: "Generais",
+        escolha2: "Faraós",
+        escolha3: "Sheiks",
+        escolha4: " Presidentes",
         resposta: 2
     },
     {
         pergunta: "Quem é o primo doido do Pato Donald?",
-        escolha1: "1- Patinhas",
-        escolha2: "2- Zé Carioca",
-        escolha3: "3- Gastão",
-        escolha4: "4- Peninha",
+        escolha1: "Patinhas",
+        escolha2: "Zé Carioca",
+        escolha3: "Gastão",
+        escolha4: " Peninha",
         resposta: 4
     },
     {
         pergunta: "Qual o desenho animado que tem um pai de família chamado Homer?",
-        escolha1: "1- Família Adams",
-        escolha2: "2- Os Finstones",
-        escolha3: "3- Denis o Pimentinha",
-        escolha4: "4- Os Simpsons",
+        escolha1: "Família Adams",
+        escolha2: "Os Finstones",
+        escolha3: "Denis o Pimentinha",
+        escolha4: " Os Simpsons",
         resposta: 4
     },
     {
         pergunta: "Qual o nome do escritor de 'Gabriela Cravo e Canela'?",
-        escolha1: "1- Graciliano Ramos",
-        escolha2: "2- Érico Veríssimo",
-        escolha3: "3- Jorge Amado",
-        escolha4: "4- Machado de Assis",
+        escolha1: "Graciliano Ramos",
+        escolha2: "Érico Veríssimo",
+        escolha3: "Jorge Amado",
+        escolha4: " Machado de Assis",
         resposta: 3
     }
 ];
@@ -143,58 +143,58 @@ let perguntas_Nvl2 = [
 let perguntas_Nvl3 = [
     {
         pergunta: "Quem reinou durante o Segundo Império Francês?",
-        escolha1: "1- Napoleão Primeiro",
-        escolha2: "2- Carlos VII",
-        escolha3: "3- Maria Antonieta",
-        escolha4: "4- Napoleão III",
+        escolha1: "Napoleão Primeiro",
+        escolha2: "Carlos VII",
+        escolha3: "Maria Antonieta",
+        escolha4: "Napoleão III",
         resposta:  4
     },
     {
         pergunta: "Em que estado brasileiro ocorreu a Guerra dos Canudos?",
-        escolha1: "1- Sergipe",
-        escolha2: "2- Minas Gerais",
-        escolha3: "3- Bahia",
-        escolha4: "4- São Paulo",
+        escolha1: "Sergipe",
+        escolha2: "Minas Gerais",
+        escolha3: "Bahia",
+        escolha4: "São Paulo",
         resposta: 3
     },
     {
         pergunta: "Que jogador da seleção foi acusado de 'amarelar' na final da copa de 98?",
-        escolha1: "1- Tafarel",
-        escolha2: "2- Romario",
-        escolha3: "3- Ronaldinho",
-        escolha4: "4- Rivaldo",
+        escolha1: "Tafarel",
+        escolha2: "Romario",
+        escolha3: "Ronaldinho",
+        escolha4: "Rivaldo",
         resposta: 3
     },
     {
         pergunta: "Qual a árvore cujo ramo simboliza a paz?",
-        escolha1: "1- Seringueira",
-        escolha2: "2- Oliveira",
-        escolha3: "3- Carvalho",
-        escolha4: "4- Ipê",
+        escolha1: "Seringueira",
+        escolha2: "Oliveira",
+        escolha3: "Carvalho",
+        escolha4: "Ipê",
         resposta: 2
     },
     {
         pergunta: "O satélite artificial Sputinik foi lançado pela União Soviética em:",
-        escolha1: "1- 1950",
-        escolha2: "2- 1955",
-        escolha3: "3- 1957",
-        escolha4: "4- 1960",
+        escolha1: "1950",
+        escolha2: "1955",
+        escolha3: "1957",
+        escolha4: "1960",
         resposta: 3
     },
     {
         pergunta: "Qual a capital do Irã?",
-        escolha1: "1- Iraque",
-        escolha2: "2- Croácia",
-        escolha3: "3- Teerã",
-        escolha4: "4- Tel Aviv",
+        escolha1: "Iraque",
+        escolha2: "Croácia",
+        escolha3: "Teerã",
+        escolha4: "Tel Aviv",
         resposta: 3
     },
     {
         pergunta: "Que pintor brasileiro pintava a beleza da mulata e a sensualidade feminina?",
-        escolha1: "1- Di Cavalcanti",
-        escolha2: "2- Cândido Portinari",
-        escolha3: "3- Volpi",
-        escolha4: "4- Tarsila do Amaral",
+        escolha1: "Di Cavalcanti",
+        escolha2: "Cândido Portinari",
+        escolha3: "Volpi",
+        escolha4: "Tarsila do Amaral",
         resposta: 1
     }
 ];
@@ -202,34 +202,34 @@ let perguntas_Nvl3 = [
 let perguntas_Nvl4 = [
     {
         pergunta: "Em que dia nasceu Bill Gates?",
-        escolha1: "1- 28 de setembro de 1957",
-        escolha2: "2- 28 de outubro de 1955",
-        escolha3: "3- 28 de fevereiro de 1960",
-        escolha4: "4- 30 de outubro de 1950",
+        escolha1: "28 de setembro de 1957",
+        escolha2: "28 de outubro de 1955",
+        escolha3: "28 de fevereiro de 1960",
+        escolha4: "30 de outubro de 1950",
         resposta: 2
     },
     {
         pergunta: "Em que ano a Microsoft foi fundada?",
-        escolha1: "1- 1973",
-        escolha2: "2- 1974",
-        escolha3: "3- 1975",
-        escolha4: "4- 1976",
+        escolha1: "1973",
+        escolha2: "1974",
+        escolha3: "1975",
+        escolha4: "1976",
         resposta: 3
     },
     {
         pergunta: "Em que dia nasceu Mark Zuckerberg?",
-        escolha1: "1- 14 de maio de 1984",
-        escolha2: "2- 14 de abril de 1985",
-        escolha3: "3- 14 de setembro de 1986",
-        escolha4: "4- 14 de maio de 1980",
+        escolha1: "14 de maio de 1984",
+        escolha2: "14 de abril de 1985",
+        escolha3: "14 de setembro de 1986",
+        escolha4: "14 de maio de 1980",
         resposta: 1
     },
     {
         pergunta: "Em que ano foi fundada a Riot Games?",
-        escolha1: "1- 2003",
-        escolha2: "2- 2009",
-        escolha3: "3- 2005",
-        escolha4: "4- 2006",
+        escolha1: "2003",
+        escolha2: "2009",
+        escolha3: "2005",
+        escolha4: "2006",
         resposta: 4
     }
 ];
@@ -284,11 +284,12 @@ function verificaNivel(){
         nivel = perguntas_Nvl3;
     }
     else if(cont === 15){
-        alert("PRÓXIMA PERGUNTA VALE 1 MILHÃO")
+        alert("PRÓXIMA PERGUNTA VALE 1 MILHÃO");
         nivel = perguntas_Nvl4;
     }
     else{
-        gameOver();
+        alert("PARABÉNS, VOCÊ ACABA DE SE TORNAR UM MILIONÁRIO!!!🎉🎉🎉");
+        return window.location.assign("index.html");
     }
     return nivel;
 }
@@ -299,18 +300,17 @@ function verificaResposta(escolha){
     if(escolha === respostaAtual){
         cont++;
         premioAcumulado = premios[cont];
-        pontos.innerHTML = "Você possui R$" + premioAcumulado + ",00";
-        erro.innerHTML = "Erro = R$" + premioAcumulado/2 + ",00"; 
-        acerto.innerHTML = "Pergunta Valendo R$" + premios[cont+1] + ",00";
-        console.log('foi');
+        pontos.innerHTML = "Você possui R$"+ premioAcumulado + ",00";
+        erro.innerHTML = "Se errar ganha R$"+ premioAcumulado/2 + ",00"; 
+        acerto.innerHTML = "Pergunta valendo R$"+ premios[cont+1] + ",00";
         criaPergunta();
     }   
     else{
-        gameOver();
+        gameOverLose();
     }
 }
 
-//Pula a questão e  a retira do array
+//Pula a questão e a retira do array
 function pular(){
     if(contPulos < 3){
         contPulos++;
@@ -324,11 +324,16 @@ function pular(){
 
 //Acaba o jogo e devolve o total ganho
 function parar(){
-    gameOver();
+    gameOverWin();
 }
 
-function gameOver(){
+function gameOverWin(){
     alert("Parabéns, você ganhou R$" + premioAcumulado + ",00");
+    return window.location.assign("index.html");
+}
+
+function gameOverLose(){
+    alert("Oh não, parece que a respota está errada, mas pelo menos você levou R$" + premioAcumulado/2 + ",00");
     return window.location.assign("index.html");
 }
 
